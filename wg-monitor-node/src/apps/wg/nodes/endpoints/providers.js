181eb9153658ad56880627e6fdb7c5f06ad183e2
Gabriel Bernedo Kaseng <84144762+Darkfrost-1107@users.noreply.github.com>
2025-02-26 10:39:50 -0500

    feat: add getConfig to privder

--- wg-monitor-node/src/apps/wg/nodes/endpoints/providers.js
+++ wg-monitor-node/src/apps/wg/nodes/endpoints/providers.js
@@ -1,4 +1,4 @@
-const {router} = app.CreateControllerApp({
+const {router, controller} = app.CreateControllerApp({
   findall: {
     middleware: [auth.authMiddleWare, auth.includeRoles(["SUPERADMIN"])],
     process: (query) => query
@@ -22,5 +22,38 @@ const {router} = app.CreateControllerApp({
     process: (params) => params
   }
 }, db.CreateClient().providerNode)
+
+async function getWgProviderConfig(req, res){
+  const {owner, id} = req.params;
+  const provider = await controller.find({
+    where: {
+      id,
+      owner: {
+        is: {
+          id: owner
+        }
+      }
+    },
+    include:{
+      clientNodes: true
+    }
+  })
+
+  if(provider === null){
+    res.status(404).json({
+      detail: "Not-Found"
+    })
+    return;
+  }
+  
+  res.append('Content-Disposition', `attachment; filename="${ provider.interfaceName }.conf"`);
+  res.append('Content-Type', 'text/plain');
+
+  const config = await wg.getWgProviderConfig(provider)
+  res.send(config)
+}
+
+router.get("/:owner/:id/getConfig", [auth.hasOwnership], getWgProviderConfig)
+router.post("/:owner/:id/getConfig", [auth.APIAuth], getWgProviderConfig)
 
 module.exports = router;
