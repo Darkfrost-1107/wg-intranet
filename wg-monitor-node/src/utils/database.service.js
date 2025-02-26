626bd285dbf2d11c80fca89d49b4bea0826b8a7e
Gabriel Bernedo Kaseng <84144762+Darkfrost-1107@users.noreply.github.com>
2025-02-26 10:35:42 -0500

    feat: add container for local PostgreSQL database

--- wg-monitor-node/src/utils/database.service.js
+++ wg-monitor-node/src/utils/database.service.js
@@ -1,8 +1,26 @@
+/**
+ * @file database.service.js
+ * @description Conexión con la base de datos
+ * Resumen de todo lo relacionado a la base de datos
+ */
+
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const { PrismaPg } = require('@prisma/adapter-pg');
 
+const connectionString = process.env.DATABASE_URL
+
+const pool = new Pool({ connectionString })
+
+const adapter = new PrismaPg(pool)
+/**
+ * Crear un cliente de base de datos
+ * 
+ * [Pendiente | TODO]:
+ * - [ ] Reconsiderar, si debería quedar como factory o convertirlo a un singleton
+ */
 function CreateClient(){
-  return new PrismaClient();
+  return new PrismaClient({adapter});
 }
 
 async function CloseClient(client){
