6c87193573cb8245005b9b45307d8aba2ee82869
Gabriel Bernedo Kaseng <84144762+Darkfrost-1107@users.noreply.github.com>
2025-02-26 11:11:56 -0500

    fix: delete migrations

--- wg-monitor-node/prisma/migrations/20250223045104_name_roles/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `userType` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "userType",
-ADD COLUMN     "roles" "UserRoles"[] DEFAULT ARRAY['USER']::"UserRoles"[];
