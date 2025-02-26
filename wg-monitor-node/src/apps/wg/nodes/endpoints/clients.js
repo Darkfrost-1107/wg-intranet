e5e06d020f66d7feac194ae6c91802f29e98e9f2
Gabriel Bernedo Kaseng <84144762+Darkfrost-1107@users.noreply.github.com>
2025-02-26 10:40:29 -0500

    feat: add getConfig and assign gateway to public providers

--- wg-monitor-node/src/apps/wg/nodes/endpoints/clients.js
+++ wg-monitor-node/src/apps/wg/nodes/endpoints/clients.js
@@ -1,4 +1,4 @@
-const {router} = app.CreateControllerApp({
+const {router, controller} = app.CreateControllerApp({
   findall: {
     middleware: [auth.authMiddleWare, auth.includeRoles(["SUPERADMIN"])],
     process: (query) => query
@@ -23,4 +23,114 @@ const {router} = app.CreateControllerApp({
   }
 }, db.CreateClient().clientNode)
 
+async function getWgClientConfig(req, res){
+  const {owner, id} = req.params;
+  const client = await controller.find({
+    where: {
+      id,
+      owner: {
+        is: {
+          id: owner
+        }
+      }
+    },
+    include:{
+      gateway: true
+    }
+  })
+
+  if(client === null){
+    res.status(404).json({
+      detail: "Not-Found"
+    })
+    return;
+  }
+  
+  res.append('Content-Disposition', `attachment; filename="${ client.interfaceName }.conf"`);
+  res.append('Content-Type', 'text/plain');
+
+  const config = await wg.getWgClientConfig(client)
+  res.send(config)
+}
+
+router.get("/:owner/:id/getConfig", [auth.hasOwnership], getWgClientConfig)
+router.post("/:owner/:id/getConfig", [auth.APIAuth], getWgClientConfig)
+
+router.post("/:owner/:id/assignGateway", [auth.authMiddleWare, auth.includeRoles(["SUPERADMIN", "USER"]), auth.hasOwnership], async (req, res) => {
+  const {owner, id} = req.params;
+  const {gatewayId} = req.body;
+  if(gatewayId === undefined){
+    res.status(400).json({
+      detail: "Bad-Request"
+    })
+    return 
+  }
+
+  const client = await controller.find({
+    where: {
+      id,
+      owner: {
+        is: {
+          id: owner
+        }
+      }
+    }
+  })
+
+  if(client === null){
+    res.status(404).json({
+      detail: "Not-Found Client"
+    })
+    return;
+  }
+
+  const gateway = await db.CreateClient().gateway.findFirst({
+    where: {
+      id: gatewayId
+    }
+  })
+
+  if(gateway === null){
+    res.status(404).json({
+      detail: "Not-Found Provider"
+    })
+    return;
+  }
+
+  if(gateway.access == "PRIVATE"){
+    res.status(403).json({
+      detail: "Forbidden"
+    })
+    return;
+  }
+
+  if(gateway.access == "PUBLIC"){
+    const result = await controller.update({
+      where: {
+        id,
+        owner: {
+          is: {
+            id: owner
+          }
+        }
+      },
+      data: {
+        gateway: {
+          connect: {
+            id: gatewayId
+          }
+        }
+      }
+    })
+    
+    res.json({
+      detail: "Assigned",
+      data: result
+    })
+    return;
+  }
+  res.status(500).json({
+    detail: "Internal-Server-Error"
+  })
+})
 module.exports = router;
